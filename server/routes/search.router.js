41f48728389a66bc439031f8008e9d3b8f9daa0d
dylanroets <110434197+dylanroets@users.noreply.github.com>
2023-01-09 14:29:22 -0600

    tinkered with API search according to documentation

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -8,24 +8,22 @@ router.get('/:string', (req, res) => {
     console.log('search players router: ', req.params.string);
     const searchString = req.params.string;
     
+
     const options = {
         method: 'GET',
+        url: 'https://api-american-football.p.rapidapi.com/players',
+        params: {search: searchString},
         headers: {
             'X-RapidAPI-Key': `${process.env.NFL_API_KEY}`,
             'X-RapidAPI-Host': 'api-american-football.p.rapidapi.com'
-        }
-    };
+            }
+        };
 
-    const query = axios.get(  `https://api-american-football.p.rapidapi.com/players?search=${searchString}`, options  )
-    
-
-    pool
-    .query(query, searchString)
-    .then(result => {res.send(result.rows)})
-    .catch(err => {
-        console.log('Error in GET team players', err);
-        res.sendStatus(500)
-    })
+        axios.request(options).then(function (response) {
+            console.log(response.data);
+        }).catch(function (error) {
+            console.error(error);
+        });
 });
 
 
